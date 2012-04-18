154c21067414d92eb0b2cf4db3ae562c96c56542
W. Trevor King <wking@tremily.us>
2012-04-18 13:22:58 -0400

    Change my email address from drexel.edu to tremily.us.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ _this_dir = os.path.dirname(__file__)
 setup(name='pypid',
       version=__version__,
       maintainer='W. Trevor King',
-      maintainer_email='wking@drexel.edu',
+      maintainer_email='wking@tremily.us',
       url='http://blog.tremily.us/posts/pypid/',
       download_url='http://git.tremily.us/?p=pypid.git;a=snapshot;h=v{};sf=tgz'.format(
         __version__),
